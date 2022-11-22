459d6168c54e6d7423eb3828e3173296d1335780
Aumy <aumy@outlook.jp>
2022-11-22 19:28:12 +0900

    Enable to build with Nix

--- flake.nix
+++ flake.nix
@@ -7,9 +7,31 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
-        packages = flake-utils.lib.flattenTree { };
+        packages = flake-utils.lib.flattenTree rec {
+          nihera = pkgs.buildDotnetModule rec {
+            pname = "nihera-chang";
+            version = "0.0.1";
+
+            src = ./.;
+
+            projectFile = "NiheraChang/NiheraChang.fsproj";
+            nugetDeps = ./nix/deps.nix;
+
+            dotnet-sdk = pkgs.dotnetCorePackages.sdk_6_0;
+            dotnet-runtime = pkgs.dotnetCorePackages.runtime_6_0;
+
+            runtimeDeps = with pkgs; [ ffmpeg libopus libsodium ];
+          };
+        };
         devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.dotnet-sdk_6 pkgs.ffmpeg pkgs.pkg-config pkgs.libopus pkgs.libsodium];
+          buildInputs = [
+            pkgs.dotnet-sdk_6
+            pkgs.nuget-to-nix
+
+            pkgs.ffmpeg
+            pkgs.libopus
+            pkgs.libsodium
+          ];
           shellHook = ''
             export LD_LIBRARY_PATH=${pkgs.libopus}/lib:${pkgs.libsodium}/lib
           '';
