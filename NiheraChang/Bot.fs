d607b4063d3ed07e9cc6a6d878e520d7dfc33b2f
Aumy <aumy@outlook.jp>
2023-01-22 17:44:24 +0900

    Add leave command

--- NiheraChang/Bot.fs
+++ NiheraChang/Bot.fs
@@ -108,10 +108,43 @@ type InteractionContext with
                 return Some(connection)
         }
 
+    member self.CatchError(t: TaskResult<unit, string>) =
+        let reportError msg =
+            self.RespondAsync $"Error: {msg}"
+
+        task {
+            let! result = t
+
+            match result with
+            | Error e ->
+                printfn "%s" e
+                do! reportError e
+            | Ok _ ->
+                printfn "Command executed successfully."
+        }
+
 
 
 type MusicSlash() =
     inherit ApplicationCommandModule()
+
+    [<SlashCommand(name = "leave", description = "Leave the VC")>]
+    member _.Leave(ctx: InteractionContext) : Task =
+        taskResult {
+            let vnext = ctx.Client.GetVoiceNext()
+
+            let vnc = vnext.GetConnection(ctx.Guild)
+
+            if vnc = null then
+                do! ctx.RespondAsync $"VCに入っていません"
+                return ()
+
+            vnc.Disconnect()
+
+            do! ctx.RespondAsync "切断しました"
+        }
+        |> ctx.CatchError
+        :> Task
 
     [<SlashCommand(name = "play", description = "Play audio from niconico")>]
     member _.Play(ctx: InteractionContext, [<Option("id", "Video id to play. Example: sm9")>] id: string) : Task =
@@ -128,5 +161,6 @@ type MusicSlash() =
                 }
 
             do!
-                result |> Result.either (Task.FromResult) (fun e -> ctx.RespondAsync $"{e}" |> Task.ofUnit)
+                result
+                |> Result.either (Task.FromResult) (fun e -> ctx.RespondAsync $"{e}" |> Task.ofUnit)
         }
